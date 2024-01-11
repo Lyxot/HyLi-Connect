dc02cba8031dc100da53cb8928b74438feeee379
A-JiuA <1169942595@qq.com>
2024-01-12 00:40:02 +0800

    ui: About Page

--- app/src/main/java/xyz/hyli/connect/ui/pages/SettingsPage.kt
+++ app/src/main/java/xyz/hyli/connect/ui/pages/SettingsPage.kt
@@ -1,6 +1,11 @@
 package xyz.hyli.connect.ui.pages
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,26 +13,38 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.getString
 import androidx.navigation.NavHostController
 import compose.icons.CssGgIcons
 import compose.icons.LineAwesomeIcons
 import compose.icons.cssggicons.AppleWatch
+import compose.icons.cssggicons.CornerRightUp
 import compose.icons.cssggicons.GlobeAlt
 import compose.icons.cssggicons.Laptop
 import compose.icons.lineawesomeicons.DesktopSolid
+import compose.icons.lineawesomeicons.Github
 import compose.icons.lineawesomeicons.MobileAltSolid
 import compose.icons.lineawesomeicons.TvSolid
+import xyz.hyli.connect.BuildConfig
 import xyz.hyli.connect.R
 import xyz.hyli.connect.composeprefs3.PrefsScreen
 import xyz.hyli.connect.composeprefs3.prefs.DropDownPref
@@ -36,15 +53,17 @@ import xyz.hyli.connect.composeprefs3.prefs.EditTextPref
 import xyz.hyli.connect.composeprefs3.prefs.ListPref
 import xyz.hyli.connect.composeprefs3.prefs.SwitchPref
 import xyz.hyli.connect.datastore.PreferencesDataStore
+import xyz.hyli.connect.ui.theme.HyliConnectTypography
 import xyz.hyli.connect.ui.viewmodel.HyliConnectViewModel
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SettingsScreen(viewModel: HyliConnectViewModel, navController: NavHostController, currentSelect: MutableState<Int>) {
+    val context = LocalContext.current
     PrefsScreen(dataStore = PreferencesDataStore.dataStore,
         modifier = Modifier
             .fillMaxSize()
-            .padding(start = 12.dp, end = 12.dp)) {
+            .padding(horizontal = 12.dp)) {
         prefsItem {
             Row(modifier = Modifier
                 .fillMaxWidth()) {
@@ -56,7 +75,7 @@ fun SettingsScreen(viewModel: HyliConnectViewModel, navController: NavHostContro
             }
             Spacer(modifier = Modifier.height(12.dp))
         }
-        prefsGroup("Basic") {
+        prefsGroup(getString(context, R.string.page_settings_basic)) {
             prefsItem {
                 EditTextPref(
                     title = stringResource(id = R.string.page_settings_nickname),
@@ -103,7 +122,7 @@ fun SettingsScreen(viewModel: HyliConnectViewModel, navController: NavHostContro
                 )
             }
         }
-        prefsGroup("Functions") {
+        prefsGroup(getString(context, R.string.page_settings_functions)) {
             prefsItem {
                 SwitchPref(key = "is_stream", title = stringResource(id = R.string.page_settings_stream))
             }
@@ -137,6 +156,67 @@ fun SettingsScreen(viewModel: HyliConnectViewModel, navController: NavHostContro
                     title = stringResource(id =  R.string.page_settings_notification_forward),
                     summary = stringResource(id = R.string.page_settings_notification_forward_summary)
                 )
+            }
+        }
+        prefsGroup(getString(context, R.string.page_settings_other)) {
+            prefsItem {
+                Spacer(modifier = Modifier.height(12.dp))
+                Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+//                    implementation 'com.google.accompanist:accompanist-drawablepainter:0.32.0'
+//                    val icon = context.packageManager.getApplicationIcon(BuildConfig.APPLICATION_ID)
+//                    Icon(painter = DrawablePainter(icon), contentDescription = null, modifier = Modifier.size(144.dp), tint = Color.Unspecified)
+                    Box {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_launcher_background),
+                            contentDescription = null,
+                            modifier = Modifier.clip(CircleShape).size(144.dp),
+                            tint = Color.Unspecified
+                        )
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                            contentDescription = null,
+                            modifier = Modifier.size(144.dp),
+                            tint = Color.Unspecified
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Text(text = stringResource(id = R.string.app_name),
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 34.sp,
+                        lineHeight = 40.sp,
+                        letterSpacing = 0.sp)
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Text(text = stringResource(id = R.string.page_settings_version) + ": " + BuildConfig.VERSION_NAME + " (" + BuildConfig.VERSION_CODE + ")")
+                    Text(text = stringResource(id = R.string.author), style = HyliConnectTypography.bodyLarge)
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Row(modifier = Modifier.clickable { context.startActivity(Intent(context, AboutPage::class.java)) }.padding(vertical = 6.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(Icons.Default.Info, contentDescription = null, modifier = Modifier.size(24.dp))
+                        Text(text = stringResource(id = R.string.page_settings_about) + " " + stringResource(id = R.string.app_name), modifier = Modifier.padding(horizontal = 6.dp))
+                    }
+                    Row(modifier = Modifier
+                        .padding(start = 4.dp)
+                        .clickable { context.startActivity(Intent().apply {
+                            action = Intent.ACTION_VIEW
+                            data = Uri.parse(getString(context, R.string.url_github))
+                        }) },
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            LineAwesomeIcons.Github,
+                            contentDescription = null,
+                            modifier = Modifier.size(24.dp)
+                        )
+                        Text(text = "A-JiuA/HyliConnect", modifier = Modifier.padding(horizontal = 6.dp))
+                        Icon(
+                            CssGgIcons.CornerRightUp,
+                            contentDescription = null,
+                            modifier = Modifier.size(12.dp)
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(144.dp))
             }
         }
     }
