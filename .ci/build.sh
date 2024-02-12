d232117f5309d95ac986b54924d11695a7940492
Lxyot <1169942595@qq.com>
2024-02-12 15:27:08 +0800

    chore: upload mappings

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,11 +1,11 @@
-
+#!/bin/bash
 if [ ! -z "${RELEASE_KEY}" ]; then
     export ANDROID_KEYSTORE_FILE="${GITHUB_WORKSPACE}/release-key.jks"
     base64 --decode <<< "${RELEASE_KEY}" > "${ANDROID_KEYSTORE_FILE}"
 fi
 
 chmod +x gradlew
-./gradlew assembleRelease
+./gradlew assembleRelease || exit 1
 
 if [ ! -z "${RELEASE_KEY}" ]; then
     rm "${ANDROID_KEYSTORE_FILE}"
