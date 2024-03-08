94283cc67d47001d94067f74a88114805a275669
Lxyot <1169942595@qq.com>
2024-03-08 23:48:28 +0800

    feat: send server port

--- app/src/main/java/xyz/hyli/connect/bean/DeviceInfo.kt
+++ app/src/main/java/xyz/hyli/connect/bean/DeviceInfo.kt
@@ -1,12 +1,13 @@
 package xyz.hyli.connect.bean
 
 data class DeviceInfo(
-    val api_version: Int,
-    val app_version: Int,
-    val app_version_name: String,
+    val apiVersion: Int,
+    val appVersion: Int,
+    val appVersionName: String,
     val platform: String,
     val uuid: String,
     val nickname: String,
-    val ip_address: MutableList<String>,
-    val port: Int
+    val ipAddress: MutableList<String>,
+    val connectedPort: Int? = null,
+    val serverPort: Int
 )
