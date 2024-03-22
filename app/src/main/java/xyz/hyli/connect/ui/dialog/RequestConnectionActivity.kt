c6f9c8ec2abbba6e2edd317d42c892961f5bcaf2
Lxyot <1169942595@qq.com>
2024-03-22 22:54:38 +0800

    feat: apply dynamic color in RequestConnectionActivity

--- app/src/main/java/xyz/hyli/connect/ui/dialog/RequestConnectionActivity.kt
+++ app/src/main/java/xyz/hyli/connect/ui/dialog/RequestConnectionActivity.kt
@@ -1,20 +1,31 @@
 package xyz.hyli.connect.ui.dialog
 
-import android.content.DialogInterface
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.provider.Settings
-import android.view.Gravity
 import android.view.Window
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AlertDialog
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
+import kotlinx.coroutines.delay
 import xyz.hyli.connect.R
 import xyz.hyli.connect.bean.DeviceInfo
 import xyz.hyli.connect.socket.SocketUtils
+import xyz.hyli.connect.ui.theme.HyLiConnectColorScheme
 import xyz.hyli.connect.ui.theme.HyLiConnectTheme
+import xyz.hyli.connect.ui.theme.HyLiConnectTypography
 
 class RequestConnectionActivity : ComponentActivity() {
     private lateinit var dialog: AlertDialog
@@ -34,53 +45,86 @@ class RequestConnectionActivity : ComponentActivity() {
 
         setContent {
             HyLiConnectTheme {
-                if ((ip.isNullOrEmpty().not() && nickname.isNullOrEmpty().not() && uuid.isNullOrEmpty().not() && api_version != 0 && app_version != 0 && app_version_name.isNullOrEmpty().not() && platform.isNullOrEmpty().not()) &&
-                    Settings.canDrawOverlays(this)
-                ) {
+                val showRequestDialog = remember { mutableStateOf(true) }
+                if ((ip.isNullOrEmpty().not() && nickname.isNullOrEmpty().not() && uuid.isNullOrEmpty().not() && api_version != 0 && app_version != 0 && app_version_name.isNullOrEmpty().not() && platform.isNullOrEmpty().not())) {
                     val IP_Address = ip!!.substring(1, ip.length)
-                    dialog = MaterialAlertDialogBuilder(this)
-                        .setTitle(getString(R.string.dialog_connect_request_title))
-                        .setMessage("${getString(R.string.dialog_connect_request_message, nickname, IP_Address)}\n\nUUID: $uuid")
-                        .setPositiveButton(getString(R.string.dialog_connect_request_accept)) { dialog, _ ->
-                            val deviceInfo = DeviceInfo(
-                                api_version,
-                                app_version,
-                                app_version_name ?: "",
-                                platform ?: "",
-                                uuid ?: "",
-                                nickname ?: "",
-                                mutableListOf(ip.substring(1, ip.length).split(":")[0]),
-                                ip.substring(1, ip.length).split(":").last().toInt(),
-                                server_port
-                            )
-                            SocketUtils.acceptConnection(ip, deviceInfo)
-                            dialog.dismiss()
-                        }
-                        .setNegativeButton(getString(R.string.dialog_connect_request_reject_countdown, 30000 / 1000)) { dialog, which ->
-                            SocketUtils.rejectConnection(ip)
-                            dialog.dismiss()
-                        }
-                        .setCancelable(true)
-                        .create()
-                    val timer = object : CountDownTimer(30000, 1000) {
-                        override fun onTick(millisUntilFinished: Long) {
-                            dialog.getButton(DialogInterface.BUTTON_NEGATIVE).text = getString(R.string.dialog_connect_request_reject_countdown, millisUntilFinished / 1000)
-                        }
-                        override fun onFinish() {
-                            dialog.dismiss()
-                        }
+                    if (showRequestDialog.value) {
+                        AlertDialog(
+                            modifier = Modifier.fillMaxWidth(0.9f),
+                            onDismissRequest = {
+                                showRequestDialog.value = false
+                                finish() },
+                            title = {
+                                Column(modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp)) {
+                                    Text(stringResource(id = R.string.dialog_connect_request_title), style = HyLiConnectTypography.titleLarge)
+                                    Text(
+                                        text = stringResource(id = R.string.dialog_connect_request_message, nickname!!, IP_Address),
+                                        style = HyLiConnectTypography.bodyLarge,
+                                        modifier = Modifier.padding(top = 6.dp),
+                                    )
+                                    Text(
+                                        text = uuid ?: "",
+                                        style = HyLiConnectTypography.bodySmall,
+                                        modifier = Modifier.padding(top = 6.dp),
+                                        color = HyLiConnectColorScheme().outline
+                                    )
+                                }
+                            },
+                            confirmButton = {
+                                TextButton(
+                                    modifier = Modifier.padding(end = 16.dp),
+                                    onClick = {
+                                        val deviceInfo = DeviceInfo(
+                                            api_version,
+                                            app_version,
+                                            app_version_name ?: "",
+                                            platform ?: "",
+                                            uuid ?: "",
+                                            nickname ?: "",
+                                            mutableListOf(ip.substring(1, ip.length).split(":")[0]),
+                                            ip.substring(1, ip.length).split(":").last().toInt(),
+                                            server_port
+                                        )
+                                        SocketUtils.acceptConnection(ip, deviceInfo)
+                                        showRequestDialog.value = false
+                                        finish()
+                                    }
+                                ) {
+                                    Text(stringResource(id = R.string.dialog_connect_request_accept), style = HyLiConnectTypography.bodyLarge)
+                                }
+                            },
+                            dismissButton = {
+                                TextButton(
+                                    modifier = Modifier.padding(end = 16.dp),
+                                    onClick = {
+                                        SocketUtils.rejectConnection(ip)
+                                        showRequestDialog.value = false
+                                        finish()
+                                    }
+                                ) {
+                                    val countDown = remember { mutableIntStateOf(30) }
+                                    LaunchedEffect(countDown.intValue) {
+                                        delay(1000)
+                                        if (countDown.intValue > 0) {
+                                            countDown.intValue -= 1
+                                        } else {
+                                            SocketUtils.rejectConnection(ip)
+                                            showRequestDialog.value = false
+                                            finish()
+                                        }
+                                    }
+                                    Text("${stringResource(R.string.dialog_connect_request_reject_countdown)} (${countDown.intValue})", style = HyLiConnectTypography.bodyLarge)
+                                }
+                            },
+                            properties = DialogProperties(usePlatformDefaultWidth = false),
+                            containerColor = HyLiConnectColorScheme().background
+                        )
+                    } else {
+                        finish()
                     }
-                    dialog.setOnDismissListener {
-                        timer.cancel()
-                    }
-                    dialog.window?.setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY)
-                    dialog.window?.attributes?.height = WindowManager.LayoutParams.WRAP_CONTENT
-                    dialog.window?.attributes?.width = WindowManager.LayoutParams.WRAP_CONTENT
-                    dialog.window?.attributes?.gravity = Gravity.CENTER
-                    dialog.show()
-                    timer.start()
+                } else {
+                    finish()
                 }
-                finish()
             }
         }
     }
