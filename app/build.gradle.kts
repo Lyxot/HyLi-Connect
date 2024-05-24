fe0ec8f5946f0499d3975977021f112a2e4728cf
Lxyot <1169942595@qq.com>
2024-05-24 18:43:59 +0800

    chore: migrate to Kotlin 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id("com.android.application")
     id("com.didiglobal.booster")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.protobuf")
     id("io.gitlab.arturbosch.detekt")
     id("org.lsposed.lsplugin.jgit") version "1.1"
@@ -118,9 +119,6 @@ android {
         aidl = true
         buildConfig = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -175,7 +173,7 @@ android {
 dependencies {
 //    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.5")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.23"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.0.0"))
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
